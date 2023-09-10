9d7006072ee19fa200aeabfbd82f22cd92bbdf66
Jelmer Hinssen <jelmerhinssen@hotmail.com>
2023-09-10 22:55:25 +0200

    camera controller

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
 import math
+import glm
 
 vertices = (
     (1, -1, -1),
@@ -54,6 +55,90 @@ def sgn(x):
         return 1
     else:
         return 0
+    
+class Camera:
+    def __init__(self, size):
+        self.mode = MODE_3D_PERSPECTIVE
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.rx = 0
+        self.ry = 0
+        self.rz = 0
+        self.size = size
+    def apply(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        zn = 0.01
+        zf = 10
+        fovy = 80
+        aspect = self.size[0] / self.size[1]
+        # aspect = 1
+        if self.mode == MODE_3D_PERSPECTIVE:
+            # Create a perspective matrix such that:
+            # positive x is to the right
+            # positive z is up
+            # positive y is outward of the camera (i.e. the direction you are looking at)
+
+            gluPerspective(fovy, aspect, zn, zf)
+            m = [1, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+            glMultMatrixf(m)
+        elif self.mode == MODE_3D_ORTHO:
+            # Create a orthographic matrix such that:
+            # positive x is to the right
+            # positive z is up
+            # positive y is outward of the camera (i.e. the direction you are looking at)
+            # Objects at y = 1 appear the same size as with the MODE_3D_PERSPECTIVE
+            f = math.tan(math.radians(fovy / 2))
+            glOrtho(-f * aspect, f * aspect, -f, f, zn, zf)
+            m = [1, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+            glMultMatrixf(m)
+        elif self.mode == MODE_2D_TOP:
+            # Create a orthographic matrix such that:
+            # positive x is to the right
+            # positive y is up
+            # negative z is the direction you are looking at
+            # Objects at z = 1 appear the same size as with the MODE_3D_PERSPECTIVE
+            # f = math.tan(math.radians(fovy / 2))
+            f = 6
+            glOrtho(-f * aspect, f * aspect, -f, f, zn, zf)
+            m = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+            glMultMatrixf(m)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glRotatef(-self.rz, 0, 0, 1)
+        glRotatef(-self.ry, 0, 1, 0)
+        glRotatef(-self.rx, 1, 0, 0)
+        glTranslatef(-self.x, -self.y, -self.z)
+    
+    def update(self, dt):
+        dir = glm.vec3(0)
+        pressed = pygame.key.get_pressed()
+        if pressed[K_w]: dir.y += 1
+        if pressed[K_s]: dir.y -= 1
+        if pressed[K_d]: dir.x += 1
+        if pressed[K_a]: dir.x -= 1
+        if pressed[K_SPACE]: dir.z += 1
+        if pressed[K_LSHIFT] or pressed[K_RSHIFT]: dir.z -= 1
+
+        if glm.dot(dir, dir) < 0.1: return
+        dir = glm.normalize(dir)
+        dir *= dt
+
+        self.moveHorizontal(dir)
+
+    def moveHorizontal(self, dir):
+        transform = glm.mat4(1)
+        transform = glm.rotate(glm.radians(self.rz), glm.vec3(0, 0, 1))
+        xaxis = glm.vec3(transform * glm.vec4(1, 0, 0, 1))
+        transform = glm.rotate(glm.radians(self.rx), xaxis) * transform
+        forward = transform * glm.vec4(0, 1, 0, 1)
+        right = glm.rotate(glm.radians(-90), glm.vec3(0, 0, 1)) * forward
+        up = glm.vec4(0, 0, 1, 1)
+        hordir = dir.x * right + dir.y * forward + dir.z * up
+        self.x += hordir.x
+        self.y += hordir.y
+        self.z += hordir.z
 
 class Display:
     def __init__(self):
@@ -63,12 +148,18 @@ class Display:
         self.font = pygame.font.Font(None, 30)
         self.mode = MODE_3D_PERSPECTIVE
         self.setupGL()
+        self.cameraA = Camera(display)
+        self.cameraB = Camera(display)
+        self.cameraB.mode = MODE_2D_TOP
+        self.cameraB.z = 5
+        self.cameraB.y = 0
         self.resize(display)
         self.clock = Clock()
         self.overlayTextureID = glGenTextures(1)
         self.tpt = 0
         self.x = 0
         self.dx = 1
+
 
     def setupGL(self):
         glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -140,18 +231,26 @@ class Display:
         glEnd()
         glPopMatrix()
 
+    def renderScene(self):
+        self.drawCube(0, 0, 0)
+        self.drawCube(0, 1, -0.3)
+
     def render(self):
         self.beginFrame()
-        self.drawCube(0, 0, 0)
-        self.drawCube(0, 1, 0)
-        glPushMatrix()
-        glTranslate(self.x, 0, 0)
-        # self.drawLines()
+        glViewport(0, 0, int(self.size[0] / 2), self.size[1])
+        self.cameraA.apply()
+        self.renderScene()
+        self.drawCamera(self.cameraB)
+        glViewport(int(self.size[0] / 2), 0, int(self.size[0] / 2), self.size[1])
+        self.cameraB.apply()
+        self.renderScene()
+        self.drawCamera(self.cameraA)
+        glViewport(0, 0, self.size[0], self.size[1])
         if self.tpt != 0:
             self.drawString(str(int(1 / self.tpt)), (0, 0))
         self.renderOverlay()
         self.endFrame()
-        glPopMatrix()
+        
     
     def drawLines(self):
         glColor3f(1, 1, 1)
@@ -169,63 +268,38 @@ class Display:
         glVertex3f( 0.7,  1.2, -0.7)
         glEnd()
 
+    def drawCamera(self, camera):
+        glPushMatrix()
+        glTranslatef(camera.x, camera.y,camera.z)
+        glRotatef(camera.rx, 1, 0, 0)
+        glRotatef(camera.ry, 0, 1, 0)
+        glRotatef(camera.rz, 0, 0, 1)
+        glScale(0.3, 0.3, 0.3)
+        glBegin(GL_LINES)
+        glVertex3f(0, 0, 0.1)
+        glVertex3f(0, 1, 0.1)
+        glVertex3f(-0.3, 0.7, 0.1)
+        glVertex3f(0, 1, 0.1)
+        glVertex3f(0.3, 0.7, 0.1)
+        glVertex3f(0, 1, 0.1)
+        glEnd()
+        glPopMatrix()
+
     def update(self):
         dt = self.clock.dt
         self.x += 0.5 * dt * self.dx
-        if abs(self.x) > 0.3:
-            self.dx = - sgn(self.x)
+        if abs(self.x) > 1:
+            self.dx = -sgn(self.x)
+        # self.cameraB.z = self.x
+        # self.cameraA.rz += 100 * dt
+        self.cameraA.update(dt)
 
-        # glTranslate(0.1 * dt, 0, 0)
-        glRotatef(100 * dt, 0, 0, 1)
     def resize(self, size):
-        glViewport(0, 0, size[0], size[1])
         self.bgsurface = pygame.Surface(size, SRCALPHA)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        zn = 0.01
-        zf = 10
-        fovy = 80
-        aspect = size[0] / size[1]
-        # aspect = 1
-        if self.mode == MODE_3D_PERSPECTIVE:
-            # Create a perspective matrix such that:
-            # positive x is to the right
-            # positive z is up
-            # positive y is outward of the camera (i.e. the direction you are looking at)
-
-            gluPerspective(fovy, aspect, zn, zf)
-            m = [1, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
-            glMultMatrixf(m)
-        elif self.mode == MODE_3D_ORTHO:
-            # Create a orthographic matrix such that:
-            # positive x is to the right
-            # positive z is up
-            # positive y is outward of the camera (i.e. the direction you are looking at)
-            # Objects at y = 1 appear the same size as with the MODE_3D_PERSPECTIVE
-            f = math.tan(math.radians(fovy / 2))
-            glOrtho(-f * aspect, f * aspect, -f, f, zn, zf)
-            m = [1, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
-            glMultMatrixf(m)
-        elif self.mode == MODE_2D_TOP:
-            # Create a orthographic matrix such that:
-            # positive x is to the right
-            # positive y is up
-            # positive z is the direction you are looking at
-            # Objects at z = 1 appear the same size as with the MODE_3D_PERSPECTIVE
-            # f = math.tan(math.radians(fovy / 2))
-            f = 
-            glOrtho(-f * aspect, f * aspect, -f, f, zn, zf)
-            m = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1]
-            glMultMatrixf(m)
+        self.size = size
+        self.cameraA.size = (size[0] / 2, size[1])
+        self.cameraB.size = (size[0] / 2, size[1])
         
-        
-        m = glGetFloatv(GL_PROJECTION_MATRIX).transpose()
-        print(m)
-        vec = [0.5,  -1, -0.5, 1]
-        v = np.dot(m, vec)
-        print(f"{v} -> {v/v[3]}")
-
-        glMatrixMode(GL_MODELVIEW)
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -238,6 +312,15 @@ class Display:
                     self.mode += 1
                     self.mode %= MODE_COUNT
                     self.resize(pygame.display.get_window_size())
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 2:
+                self.rotstart = (self.cameraA.rx, self.cameraA.rz)
+                self.mousestart = event.pos
+            elif event.type == pygame.MOUSEMOTION and event.buttons[1] == 1:
+
+                dx = event.pos[0] - self.mousestart[0]
+                dy = event.pos[1] - self.mousestart[1]
+                self.cameraA.rx = self.rotstart[0] + dy / self.size[0] * 180
+                self.cameraA.rz = self.rotstart[1] + dx / self.size[1] * 180
         
 
 def main():
